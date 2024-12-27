ebd11745bda499ca2aea943d694d8ecfb34d5d30
fawnyler <125075848+fawnyler@users.noreply.github.com>
2024-12-27 12:08:44 +0300

    Add files via upload

---
+++ MVCLab6/MVCLab6/Observable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace MVCLab6
+{
+    public class Observable
+    {
+        protected List<IMovieObserver> observers = new List<IMovieObserver>();
+
+        public void AddObserver(IMovieObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IMovieObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        protected void NotifyObservers()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(); 
+            }
+        }
+    }
+}
